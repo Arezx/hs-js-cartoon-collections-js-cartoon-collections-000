8fd724ef5029ac345ea34ee1f8057511f140ecd0
Keginold Philip <keginoldp@hotmail.com>
2019-07-05 21:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function dwarfRollCall(dwarves) {
+var array = ["Doc", "Dopey", "Bashful", "Grumpy"]; 
+
+function dwarfRollCall(array){
+  var array2 = []
+  
+  for (let i=0; i <array.length; i++) {
+  array2.push(`${i+1}. ${array[i]} `) 
+  
+
+    
+  }return array2.join("")
+  
 }
+
+
+
+// Summon Captain Planet
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  for (let i=0; i<planeteerCalls.length; i++){
+    array.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }return array;
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
+  for (let i=0; i<words.length; i++)
+  
+  if(words[i].length>4)
+    
+    return true
+    
+    else 
+    
+    return false;
 }
+// Find the Cheese
 
-function findTheCheese (foods) {
+  function findTheCheese(foods){
+    
+    if(foods[i] === 'gouda') 
+    if(foods[i] === 'cheddar')
+    if(foods[i] === 'camembert')
+    
+     return foods[i]
+     
+     else
+     
+     return 'no cheese!';
+
 }
